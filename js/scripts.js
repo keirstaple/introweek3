25aa282389f128ba70783f3a8398b2335e646806
Keir Staple <keirstaple@comcast.net>
2016-01-22 14:24:53 -0800

    add specs and scripts and the begging of the HTML

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,43 @@
-var 
+var wrongInput = function(userNumber) {
+  if (userNumber <= 0) {
+		return "Please enter a number greater than 0";
+	}
+};
+
+var countUpTo = function(userNumber) {
+  var emptyArray = [];
+    for (var i =1; i <= userNumber; i++){
+      if (i % 5 === 0 && i % 3 === 0) {
+        emptyArray.push('pingpong');
+      } else if (i % 5 === 0){
+        emptyArray.push('pong');
+      } else if (i % 3 === 0){
+        emptyArray.push('ping');
+      } else if (i % 1 === 0){
+        emptyArray.push(i);
+      } else {
+        wrongInput(userNumber)
+      }
+    }
+    return emptyArray;
+};
+
 
 $(document).ready(function() {
+  $("form").submit(function(event) {
+    debugger;
+    var userNumber = parseInt($("input#UpTo").val());
 
+    var result = countUpTo(userNumber)
+		 	result.forEach(function(number) {
+				$(".listofresults").append("<li>" + number + "</li>");
+			});
+
+    var invalid = wrongInput(userNumber);
+
+
+	$("#result").show();
+  $(".invalid").text(invalid)
+  event.preventDefault();
+  });
 });
--- specs/specs.js
+++ specs/specs.js
@@ -1,5 +1,11 @@
-describe('', function() {
-  it("", function() {
-    expect( ()).to.equal()
-  });
+describe('countUpTo', function() {
+  it("returns 'ping-pong' for a number that's divisible by both 3 and 5, 'pong' for a number divisible by 5, and 'ping' for a number divisible by 3", function() {
+    expect(countUpTo(15)).to.equal[[1,2,'ping',4,'pong','ping',7,8,'ping','pong',11,'ping',13,14,'pingpong']];
+  })
+});
+
+describe('wrongInput', function() {
+  it("returns an invalid input message if the number is less than or equal to 0", function() {
+    expect(wrongInput(-3)).to.equal("Please enter a number greater than 0");
+  })
 });
